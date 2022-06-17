f544d09eeb2b3328863d26cc4e900d24534fc9b3
rm809 <83632267+distriful5061@users.noreply.github.com>
2022-06-17 20:20:59 +0900

    a

---
+++ AntiSpam/src/main/java/com/github/distriful5061/antispam/antispam/Antispam.java
@@ -0,0 +1,17 @@
+package com.github.distriful5061.antispam.antispam;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Antispam extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
